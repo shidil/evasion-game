21f18ea5468c015922e5bcc7f797eff005470371
Shidil Eringa <shidil@live.com>
2021-05-17 10:55:34 +0530

    Fixed wasm make file, emcc path

--- build-web.sh
+++ build-web.sh
@@ -7,6 +7,6 @@ mv build/evasion* public/game
 mv public/game/evasion.html public/game/index.html
 
 # Prepare manifest.json
-cp src/manifest.json public/game/manifest.json
+cp manifest.json public/game/manifest.json
 
 # npx serve -s public/game
